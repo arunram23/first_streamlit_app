1b3b31c994fea6b380f578b04437938d7e21d281
arunram23 <138963701+arunram23@users.noreply.github.com>
2023-07-08 16:56:14 +0530

    Update streamlit_app.py
    Add heading for data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit
 streamlit.title('My parents New Healthy Dinner')
-streamlit.header ('Breakfast Menu')
+streamlit.header ('Breakfast Favorites')
 streamlit.text ('🥣  Omega 3 & Blueberry Oatmeal')
 streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')  
 streamlit.text('🥑🍞 Avocado toast')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.header ('Build Your Own Fruit Smoothie')
 streamlit.dataframe(my_fruit_list)
+
+
