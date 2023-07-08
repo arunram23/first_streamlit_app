29c97ff2512fb6ad5a425e032cb057d21e3b35a9
arunram23 <138963701+arunram23@users.noreply.github.com>
2023-07-08 12:34:48 +0530

    Update streamlit_app.py
    Add Break fast Menu and Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('My parents New Healthy Dinner')
-Breakfast Menu
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('Kale, Spinach & Rocket Smoothie')
